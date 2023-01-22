bb67a27ebf8e420c4931d7066ebb3fb1de0ebbe5
Alireza Barani <baranali02@gmail.com>
2023-01-22 19:38:11 +0330

    Bugfix for correction.js

--- correction.js
+++ correction.js
@@ -4,16 +4,18 @@
  * Correcting the course name before search
  */
 
-let a = document.getElementsByTagName("tbody");
+let a = document.getElementById("submitBtn");
 let b = document.createElement("span");
 b.className = "gradiantButton";
 b.innerHTML =
-'<button type="button" class="button"' +
-' style="background-image: url(Pages/images/icons/sabte-darkhast.png);"' +
-' id="correctionBtn"> اصلاح نام درس</button>'
-a[6].lastElementChild.lastElementChild.prepend(b); // #FIXME using 'a.length-1' instead of 6, or find a better way
-let c = document.getElementById("correctionBtn");
-c.addEventListener("click", cnameCorrection);
+    '<button type="button" class="button"' +
+    ' style="background-image: url(Pages/images/icons/sabte-darkhast.png);"' +
+    ' id="correctionBtn"> اصلاح نام درس</button>';
+if (a != null) {
+    a.insertAdjacentElement("beforebegin", b);
+    a.insertAdjacentHTML("beforebegin", "&nbsp");
+    document.getElementById("correctionBtn").onclick = function () { cnameCorrection() };
+}
 function cnameCorrection() {
     let courseInput = document.getElementsByName("parameter(f^courseName)")[0];
     let inputValue = courseInput.value;
